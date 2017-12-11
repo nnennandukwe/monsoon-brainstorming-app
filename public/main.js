444752098f0a49651df6e40cc905fe81913fdf00
Nnenna Ndukwe <nnenna.s.ndukwe@gmail.com>
2017-12-11 10:50:42 -0500

    config of public folder & main.js

---
+++ public/main.js
@@ -0,0 +1,8 @@
+console.log("successfully loaded main.js file for CRUD execution")
+
+let stormButton = document.getElementById("stormItBtn")
+createStorm.onclick = createStorm
+
+function createStorm(){
+  let section = document.getElementById("divboard")
+}
